2109a944146c332d0f08c1f1ee8251a901345976
HamsterCat-W <2756878164@qq.com>
2021-09-29 17:23:02 +0800

    增加了商品的修改和删除功能

--- server/controller/goodsController.js
+++ server/controller/goodsController.js
@@ -71,9 +71,63 @@ const findAllGoods = async (req, res) => {
   }
 };
 
-const updateGoods = async (req, res) => {};
+const updateGoods = async (req, res) => {
+  // console.log(req);
+  let data = await goodsModel.updateOne(
+    { goodsId: req.body.goodsId },
+    req.body
+  );
+  // console.log(data);
+  if (data.modifiedCount == 1) {
+    console.log("修改成功");
+    res.send({
+      meta: {
+        status: "success",
+        code: 200,
+        msg: "修改成功",
+      },
+      data: 1,
+    });
+  } else {
+    res.send({
+      meta: {
+        status: "error",
+        code: 500,
+        msg: "修改失败",
+      },
+      data: 0,
+    });
+  }
+};
 
-const deleteGoods = async (req, res) => {};
+const deleteGoods = (req, res) => {
+  let { goodsList } = req.body;
+  try {
+    goodsList.forEach(async (item) => {
+      let data = await goodsModel.findOneAndDelete({ goodsId: item });
+      console.log("删除了一条数据");
+      console.log(data);
+    });
+    res.send({
+      meta: {
+        status: "success",
+        code: 200,
+        msg: "删除成功",
+      },
+      data: 1,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      meta: {
+        status: "error",
+        code: 500,
+        msg: "删除失败",
+      },
+      data: 0,
+    });
+  }
+};
 
 module.exports = {
   createGoods,
