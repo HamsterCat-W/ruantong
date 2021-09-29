aa635b4ff1e2911507b08f46027d7e173fbd8952
HamsterCat-W <2756878164@qq.com>
2021-09-29 16:43:23 +0800

    增加了创建商品和查询商品的功能

--- server/controller/goodsController.js
+++ server/controller/goodsController.js
@@ -1,10 +1,75 @@
 const goodsModel = require("../models/goodsModel");
 
-const createGoods = async (req, res) => {};
+const createGoods = async (req, res) => {
+  let data = await goodsModel.create(req.body);
+  console.log(data);
+  if (data) {
+    console.log("创建了一个新的商品");
 
-const findoneGoods = async (req, res) => {};
+    res.send({
+      meta: {
+        status: "success",
+        code: 200,
+        msg: "商品创建成功",
+      },
+      data: data,
+    });
+  } else {
+    res.send({
+      meta: {
+        status: "error",
+        code: 500,
+        msg: "商品创建失败",
+      },
+    });
+  }
+};
 
-const findAllGoods = async (req, res) => {};
+const findoneGoods = async (req, res) => {
+  let data = await goodsModel.findOne(req.query);
+  if (data) {
+    console.log(`查询商品${req.body.goodsId}成功`);
+    res.send({
+      meta: {
+        status: "success",
+        code: 200,
+        msg: "商品查询成功",
+      },
+      data: data,
+    });
+  } else {
+    res.send({
+      meta: {
+        status: "error",
+        code: 500,
+        msg: "商品查询失败",
+      },
+    });
+  }
+};
+
+const findAllGoods = async (req, res) => {
+  let data = await goodsModel.find();
+  if (data) {
+    console.log("查询成功");
+    res.send({
+      meta: {
+        status: "success",
+        code: 200,
+        msg: "查询成功",
+      },
+      data: data,
+    });
+  } else {
+    res.send({
+      meta: {
+        status: "error",
+        code: 500,
+        msg: "查询失败",
+      },
+    });
+  }
+};
 
 const updateGoods = async (req, res) => {};
 
