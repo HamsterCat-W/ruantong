23f4df38e01ee22747dbb9db2bed71794ddb8c4e
HamsterCat-W <2756878164@qq.com>
2021-10-18 09:41:39 +0800

    订单页面

---
+++ client/management-client/src/components/order/order.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="order-container" align="center">
+    <div class="order" v-for="(item, index) in orderList" :key="index">
+      <el-descriptions
+        :title="'订单' + (index + 1)"
+        direction="vertical"
+        :column="4"
+        border
+      >
+        <el-descriptions-item label="用户邮箱">{{
+          item.customer[0]
+        }}</el-descriptions-item>
+        <el-descriptions-item label="商家邮箱">{{
+          item.merchant[0]
+        }}</el-descriptions-item>
+        <el-descriptions-item label="商品名称">{{
+          item.goodsName[0]
+        }}</el-descriptions-item>
+        <el-descriptions-item label="商品数量">{{
+          item.goodsNum
+        }}</el-descriptions-item>
+        <el-descriptions-item label="出发地" :span="2">{{
+          item.sourcePlace
+        }}</el-descriptions-item>
+        <el-descriptions-item label="目的地址">{{
+          item.destPlace
+        }}</el-descriptions-item>
+      </el-descriptions>
+      <el-descriptions-item label="总价">{{
+        item.totalPrice
+      }}</el-descriptions-item>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      size: "",
+      orderList: [],
+    };
+  },
+  mounted() {
+    this.getOrder();
+  },
+  methods: {
+    async getOrder() {
+      this.$axiosInstance.setToken();
+      let res = await this.$request("/pv1/order/api/order", {
+        params: {
+          customerId: sessionStorage.getItem("userId"),
+        },
+      });
+
+      // console.log(res.data.data);
+      this.orderList = res.data.data;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.order-container {
+  width: 100%;
+}
+
+.order {
+  width: 80%;
+  margin-bottom: 30px;
+}
+</style>
