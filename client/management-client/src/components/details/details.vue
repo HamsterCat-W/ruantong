2500dd1bd4d35224f0b2049107ea81bd1e9fc904
HamsterCat-W <2756878164@qq.com>
2021-10-18 09:36:52 +0800

    商品详情页面连通

--- client/management-client/src/components/details/details.vue
+++ client/management-client/src/components/details/details.vue
@@ -3,30 +3,30 @@
     <el-row>
       <el-col :span="14">
         <div class="left" align="center">
-          <img
-            src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-            alt=""
-            class="left-image"
-          />
+          <img :src="goodsInfo.imgSrc" alt="" class="left-image" />
         </div>
       </el-col>
       <el-col :span="10">
         <div class="right">
-          <div class="goodsName">商品名称</div>
-          <div class="price">10.00&nbsp;元</div>
-          <div class="repertory">剩余库存: 10</div>
+          <div class="goodsName">{{ goodsInfo.goodsName }}</div>
+          <div class="price">{{ goodsInfo.price }}&nbsp;元</div>
+          <div class="repertory">剩余库存: {{ goodsInfo.repertory }}</div>
           <div class="opt">
             <div class="txt">购买数量：</div>
             <div class="sub">
-              <button class="opt-btn"><span class="tag">-</span></button>
+              <button class="opt-btn" @click="sub">
+                <span class="tag">-</span>
+              </button>
             </div>
-            <div class="num">3</div>
+            <div class="num">{{ goodsNum }}</div>
             <div class="add">
-              <button class="opt-btn"><span class="tag">+</span></button>
+              <button class="opt-btn" @click="add">
+                <span class="tag">+</span>
+              </button>
             </div>
           </div>
           <div class="addCar">
-            <el-button type="primary">加入购物车</el-button>
+            <el-button type="primary" @click="addToCar">加入购物车</el-button>
           </div>
         </div>
       </el-col>
@@ -35,7 +35,63 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      goodsId: "",
+      goodsInfo: "",
+      goodsNum: "1",
+    };
+  },
+
+  computed: {},
+
+  mounted() {
+    this.getGoods();
+  },
+  methods: {
+    async getGoods() {
+      this.goodsId = this.$route.params.goodsId;
+      this.$axiosInstance.setToken();
+      let data = await this.$request.get("/pv1/goods/api/goods", {
+        params: {
+          goodsId: this.goodsId,
+        },
+      });
+
+      this.goodsInfo = data.data.data;
+      // console.log(this.goodsInfo);
+    },
+
+    sub() {
+      if (this.goodsNum > 0) {
+        this.goodsNum--;
+      }
+    },
+    add() {
+      if (this.goodsNum < this.goodsInfo.repertory) {
+        this.goodsNum++;
+      }
+    },
+
+    async addToCar() {
+      // console.log("点击添加");
+      this.$axiosInstance.setToken();
+      let res = await this.$request.post("/pv1/goodscar/api/car", {
+        num: this.goodsNum,
+        goodsId: this.goodsId,
+        userId: sessionStorage.getItem("userId"),
+      });
+
+      if (res.data.data) {
+        this.$message({
+          message: "添加成功",
+          type: "success",
+        });
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
